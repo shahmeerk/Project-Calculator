3a071e2b640a144834489842e9356fafc267f499
Shahmeer <shahmeer.a.khan@gmail.com>
2019-07-15 23:58:15 +0500

    more stuff added

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 var firstVal = 0.0;
 var secondVal = 0.0;
+var operation = "";
+
+function storeVal(operater){
+    let displayOut = document.querySelector('#currValue');
+    firstVal = parseInt(displayOut.value);
+    operation = operater;
+    displayOut.value  = "";
+}
 
 function add(val1, val2){
-    let displayOut = document.querySelector('#currValue');
-    displayOut.value = "Hello";
+    return val1 + val2;
 }
 
 function subt(val1, val2){
@@ -18,3 +25,20 @@ function divi(val1, val2){
     return val1 / val2;
 }
 
+function calculate(){
+    let displayOut = document.querySelector('#currValue');
+    secondVal = parseInt(displayOut.value);
+
+    if (operation == "add"){
+        displayOut.value = add(firstVal, secondVal);
+    }
+    if (operation == "sub"){
+        displayOut.value = subt(firstVal, secondVal);
+    }
+    if (operation == "div"){
+        displayOut.value = divi(firstVal, secondVal);
+    }
+    if (operation == "mul"){
+        displayOut.value = mult(firstVal, secondVal);
+    }
+}
