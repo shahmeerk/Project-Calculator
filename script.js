b010eb690ba1d722e5a59d27a4ac7ca2c1c1cddb
Shahmeer <shahmeer.a.khan@gmail.com>
2019-07-15 23:34:09 +0500

    worked a bit

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-echo
+var firstVal = 0.0;
+var secondVal = 0.0;
+
+function add(val1, val2){
+    let displayOut = document.querySelector('#currValue');
+    displayOut.value = "Hello";
+}
+
+function subt(val1, val2){
+    return val1 - val2;
+}
+
+function mult(val1, val2){
+    return val1 * val2;
+}
+
+function divi(val1, val2){
+    return val1 / val2;
+}
+
